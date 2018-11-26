25f150752c8a6170bc84f422c1523069704e830a
Rubén C.C <ruben.carretero@wolterskluwer.com>
2018-11-26 11:04:16 +0100

    Proyecto unit test

---
+++ Implementation.Unit.Tests/ProcessOrderWorkflowTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Implementations;
+using Interfaces;
+using NSubstitute;
+using Xunit;
+
+namespace Implementation.Unit.Tests
+{
+    [Trait("Unit Test", "ProcessOrderWorkflow")]
+    public class ProcessOrderWorkflowTests
+    {
+        private ProcessOrderWorkflow workflow;
+        private ITracer tracer;
+        private ITrackingService trackingService;
+        private IProxyRepository proxyRepository;
+
+        public ProcessOrderWorkflowTests()
+        {
+            this.tracer = Substitute.For<ITracer>();
+            this.trackingService = Substitute.For<ITrackingService>();
+            this.proxyRepository = Substitute.For<IProxyRepository>();
+
+            this.workflow = new ProcessOrderWorkflow(this.tracer, this.trackingService, this.proxyRepository);
+        }
+
+        ~ProcessOrderWorkflowTests()
+        {
+            this.workflow = null;
+            this.tracer = null;
+            this.trackingService = null;
+            this.proxyRepository = null;
+        }
+
+        [Fact(DisplayName = "Inject tracer null reference")]
+        void Inject_tracer_null_reference()
+        {
+            var exception =
+                Record.Exception(() => new ProcessOrderWorkflow(null, this.trackingService, this.proxyRepository));
+
+            exception.Should().BeOfType<ArgumentNullException>();
+        }
+    }
+}
---
+++ Implementation.Unit.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Implementation.Unit.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Implementation.Unit.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22c76e87-8acb-4f1a-8699-541b0155bedc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
